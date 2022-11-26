6b68d02ab194357731469aa1caba2a96624e72c7
DallasVer <118686739+DallasVer@users.noreply.github.com>
2022-11-26 13:15:07 +0700

    Add files via upload

--- dart_rab6/dart_rab6_5/bin/dart_rab6_5.dart
+++ dart_rab6/dart_rab6_5/bin/dart_rab6_5.dart
@@ -1,8 +1,29 @@
 import 'dart:io';
+import 'dart:math';
 
 void main(List<String> arguments) {
-  stdout.write('Введите размер матрицы n: ');
+  stdout.write('Введите ширину матрицы n: ');
   int n = int.parse(stdin.readLineSync()!);
-  stdout.write('Введите размер матрицы m: ');
+  stdout.write('Введите высоту матрицы m: ');
   int m = int.parse(stdin.readLineSync()!);
+  List<List<int>> num = List.generate(
+      n, (i) => List.generate(m, (j) => 0 + Random().nextInt(2 - 0)));
+  List<List<int>> dabl =
+      List.generate(n, (i) => List.generate(m + 1, (j) => 0));
+  var sum = 0;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      dabl[i][j] = num[i][j];
+      if (num[i][j] > 0) {
+        sum += num[i][j];
+      }
+    }
+    if (sum % 2 != 0) {
+      dabl[i][m] += 1;
+    }
+  }
+  dabl.forEach((dabl) {
+    print(dabl);
+  });
 }
--- dart_rab6/dart_rab6_6/bin/dart_rab6_6.dart
+++ dart_rab6/dart_rab6_6/bin/dart_rab6_6.dart
@@ -7,14 +7,16 @@ void main(List<String> arguments) {
   List<double> numsN =
       List.generate(10, (j) => -10 + (10 - -10) * Random().nextDouble());
 
-  for (int i = 0; i < numsP.length; i++) {
-    if (numsP[i] < 0) {
-      numsN.add(numsP[i]);
-      numsP.removeAt(i);
-    }
-    if (numsN[i] > 0) {
-      numsP.add(numsN[i]);
-      numsN.removeAt(i);
+  for (int f = 0; f < numsN.length; f++) {
+    for (int i = 0; i < numsP.length; i++) {
+      if (numsP[i] < 0) {
+        numsN.add(numsP[i]);
+        numsP.removeAt(i);
+      }
+      if (numsN[i] > 0) {
+        numsP.add(numsN[i]);
+        numsN.removeAt(i);
+      }
     }
   }
   print(numsN);
